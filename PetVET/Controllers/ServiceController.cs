e9e8a246b573edc81aee4025a13bee3c91863ba3
marcinmika1 <marcinmika@hotmail.com>
2018-08-02 19:35:27 +0200

    Dodanie formularza AddService

---
+++ PetVET/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetVET.Models.ServiceViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PetVET.Controllers
+{
+    public class ServiceController : Controller
+    {
+        [Route("Service")]
+        public IActionResult AddService()
+        {
+            ServiceViewModel vm = new ServiceViewModel();
+
+            return View(vm);
+        }
+    }
+}
---
+++ PetVET/Models/ServiceViewModels/PetViewModel.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using PetVET.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetVET.Models.ServiceViewModels
+{
+    public class ServiceViewModel : Profile
+    {
+        [JsonProperty(PropertyName = "serviceId")]
+        public int ServiceID { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName = "serviceName")]
+        public string ServiceName { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName = "serviceCost")]
+        public int ServiceCost { get; set; }
+
+    }
+}
