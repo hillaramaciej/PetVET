f030fcfc74a5af06070161f6686a431471c0614c
marcinmika1 <marcinmika@hotmail.com>
2018-08-02 20:11:32 +0200

    Dodanie AddItem

---
+++ PetVET/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetVET.Models.ItemViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PetVET.Controllers
+{
+    public class ItemController : Controller
+    {
+        [Route("Item")]
+        public IActionResult AddItem()
+        {
+            ItemViewModel vm = new ItemViewModel();
+
+            return View(vm);
+        }
+    }
+}
