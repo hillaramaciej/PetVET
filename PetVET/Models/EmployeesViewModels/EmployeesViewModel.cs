395885c832056b4aa320f15b7f552d3e5d6ba9c1
marcinmika1 <marcinmika@hotmail.com>
2019-01-26 21:17:47 +0100

    layout

---
+++ PetVET/Models/EmployeesViewModels/EmployeesViewModel.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using PetVET.Models.Enums;
+using PetVET.Repository.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetVET.Models.EmployeesViewModels
+{
+    public class EmployeesViewModel : Profile , IViewModel
+    {
+        [JsonProperty(PropertyName = "petId")]
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName = "serviceName")]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName = "serviceCost")]
+        public double EmployeeMail { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName = "serviceCost")]
+        public double EmployeeTel { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName = "serviceType")]
+        public string EmployeeType { get; set; }
+
+
+
+
+    }
+}
